bdfbf3d1009a8e1e81bcb0d8894e05988c7be2f5
NV319649 <nelson_veras@bhdleon.com.do>
2021-05-07 13:07:40 -0400

    elephantsql posgres db added

--- src/root/settings.py
+++ src/root/settings.py
@@ -87,7 +87,8 @@ DATABASES = {
         'NAME': env('DATABASE_NAME'),
         'USER':env('DATABASE_USER'),
         'PASSWORD':env('DATABASE_PASSWORD'),
-        'HOST':env('DATABASE_HOST')
+        'HOST':env('DATABASE_HOST'),
+        'PORT':env('DATABASE_PORT')
     }
 }
 
